e020231462f9d9a86f5c38dc35977145d2c53e41
zedparman <ze.dparman@gmail.com>
2021-11-26 11:47:32 +0330

    Create profile.vue

---
+++ src/pages/profile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>profile</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+    export default {
+        name: 'next',
+        async created () {
+            const response = await axios.get('user');
+            console.log(response);
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
